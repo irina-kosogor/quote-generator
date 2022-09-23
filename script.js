6595cdd1ec6d93e026375c0d4f0d8cafc5eb05df
Irina Kosogor <i.v.kosogor@gmail.com>
2022-09-23 18:38:49 +0300

    CORS transpassed

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //Get Quote from API
 
 async function getQuote() {
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
         console.log(data);
     }
